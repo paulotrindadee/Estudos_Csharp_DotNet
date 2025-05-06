99c2faa751812c75a8177bf9418860d350c22c13
paulotrindadee <paulotrindade90@outlook.com>
2025-05-06 14:29:24 -0300

    Aula 01

---
+++ Operadores.cs
@@ -0,0 +1,61 @@
+// // Aritméticos:
+
+// // // int a = 10, b = 3;
+// // // int soma = a + b;      // 13  Soma dos Números
+// // // int sub = a - b;       // 7   Subtração dos números
+// // // int mult = a * b;      // 30  Multiplicação dos números
+// // // int div = a / b;       // 3   Divisão dos números 
+// // // int resto = a % b;     // 1   Resto de uma divisão
+
+// // // O que é o resto (%)?
+
+// // // O operador % retorna o que sobra de uma divisão inteira.
+// // // Em matemática, isso é o resto da divisão.
+
+// // // int a = 10, b = 3;
+// // // int div = a / b;     // Resultado da divisão: 3 (porque 3x3 = 9)
+// // // int resto = a % b;   // Sobra: 1 (porque 10 - 9 = 1)
+// // // 10 ÷ 3 = 3 → sobra 1
+
+// // // Onde o % é usado na prática?
+// // // 1.Verificar se um número é par ou ímpar:
+// // // int numero = 7;
+// // // if (numero % 2 == 0)
+// // // {
+// // //  Console.WriteLine("É par!");
+// // // }
+// // // else
+// // // {
+// // // Console.WriteLine("É ímpar!");
+// // // }
+
+// // // Mostrar números em colunas (ex: 3 por linha)
+// // // for (int i = 1; i <= 9; i++)
+// // // {
+// // // Console.Write(i + " ");
+// // // if (i % 3 == 0)
+// // // Console.WriteLine(); // quebra a linha a cada 3 números
+// // // }
+
+// // // Repetição cíclica (tipo relógio)
+// // // int segundos = 62;
+// // // int segundosNoMinuto = segundos % 60;  // 2 segundos restantes após 1 minuto
+
+
+// // Comparação:
+
+// // a == b  // falso
+// // a != b  // verdadeiro
+// // a > b   // verdadeiro
+// // a < b   // falso
+
+
+// // Lógicos: 
+
+// // bool resultado = (a > b) && (b > 0);  // true
+// // bool ou = (a < b) || (b > 0);         // true
+// // bool negacao = !(a == b);             // true
+
+//  Dica: && = "e", || = "ou", ! = "nega"
+
+
---
+++ TiposPrimitivos.cs
@@ -0,0 +1,71 @@
+﻿
+// int idade = 20;              // número inteiro
+// float altura = 1.75f;        // número com ponto flutuante (precisa do 'f' no final)
+// double peso = 70.5;          // número com mais precisão que o float
+// bool estaChovendo = true;    // verdadeiro ou falso
+// char letra = 'A';            // um único caractere
+// string nome = "Paulo";       // texto (sequência de caracteres)
+
+
+
+// Exemplo de atividade:
+
+// Atividade Rápida:
+// Declare uma variável pra:
+
+// Sua idade
+
+// Sua altura
+
+// Seu nome
+
+// Se você está estudando C# agora
+
+using System;
+
+class AtividadeRapida
+
+{
+static void Main() 
+{ 
+int idade = 35;
+float altura = 1.78f;
+string nome = "Paulo";
+bool EstudandoCsharp = true;
+
+Console.WriteLine($"Nome:{nome}");
+Console.WriteLine($"Idade:{idade} anos");
+Console.WriteLine($"Altura:{altura}");
+Console.WriteLine($"Estudando C# agora? {EstudandoCsharp}");
+}
+}
+
+// OBS: O ($) é o que permite interpolação de string.
+// Interpolação é um jeit mais fácil e limpo de inserir variáveis dentro de uma string.
+
+// Exemplo
+// string nome = "Paulo";
+// Console.WriteLine("Seu nome é " + nome); // concatenação tradicional
+
+// string nome = "Paulo";
+// Console.WriteLine($"Seu nome é {nome}"); // interpolação
+
+
+// Por que usar interpolação ($) é melhor?
+// Fica mais legível.
+
+// Evita ficar somando (+) strings.
+
+// Permite usar expressões direto dentro das chaves:
+
+
+// int idade = 25;
+// Console.WriteLine($"Ano que vem você terá {idade + 1} anos");
+// Sem $, você teria que fazer assim:
+
+
+// Console.WriteLine("Ano que vem você terá " + (idade + 1) + " anos");
+
+// Dica:
+// Sempre que for imprimir variáveis ou fazer contas no meio do texto, usa o $"" com {}. Seu código fica muito mais limpo e profissional.
+
